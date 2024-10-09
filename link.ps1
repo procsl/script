13488f47fcdd6d772cc35a1988a08385cabb9f39
chenshanlu <program_chen@foxmail.com>
2024-10-09 15:13:58 +0800

    autocommit

--- link.ps1
+++ link.ps1
@@ -1,35 +1,54 @@
-$adb_output = adb  devices |findStr 'f8061b9'
-if ($adb_output.Contains("f8061b9")) {
-    Write-Host "存在设备f8061b9"
+$adb_output = adb devices |findStr 'f8061b9'
+if (!$adb_output.Contains("f8061b9"))
+{
+    Write-Host "不存在设备f8061b9，请连接设备"
+    break
+}
 
-    $foreach_times = "1","2","3"
+$ps = adb -s f8061b9 shell "netstat -an|grep LISTEN |grep 8888"
+
+Write-Host "进程启动结果: "$ps
+
+if (($null -eq $ps) -or (-not $ps.Contains("LISTEN")))
+{
+    Write-Host "进程未启动，将启动进程: com.termux"
+
+    $foreach_times = "1", "2", "3"
     foreach ($letter in $foreach_times)
     {
-        $adb_start = adb shell  am start com.termux/.app.TermuxActivity
-
-        if($adb_start.Contains("Starting")){
-            Write-Host "重试启动: "+ $adb_start
-            Start-Sleep -Seconds 1
+        adb -s f8061b9 shell "am start com.termux/.app.TermuxActivity"
+        Write-Host "进程启动中..."
+        Start-Sleep -Seconds 1
+        $ps = adb -s f8061b9 shell "netstat -an|grep LISTEN |grep 8888"
+        $start = ($null -eq $ps) -or (-not $ps.Contains("LISTEN"))
+        if (!$start)
+        {
+            Write-Host "进程启动成功..."
+            break
         }
+        Write-Host "进程启动失败，1s后重试启动: "$letter"次，最多3次"
+        Start-Sleep -Seconds 1
+    }
+}
 
-    }
+$adb_list = adb -s f8061b9  forward --list
 
-    $adb_list = adb forward --list
+if (-not $adb_list.Contains("8888"))
+{
+    adb -s f8061b9  forward tcp:8888 tcp:8888
+}
 
-    if (-not $adb_list.Contains("8888")){
-        adb forward tcp:8888 tcp:8888
-    }
+if (-not $adb_list.Contains("8889"))
+{
+    adb -s f8061b9  forward tcp:8889 tcp:8889
+}
 
-    if (-not $adb_list.Contains("8889")){
-        adb forward tcp:8889 tcp:8889
-    }
-   
-    if (-not $adb_list.Contains("8022")){
-        adb forward tcp:8022 tcp:8022
-    }
-	
-	if (-not $adb_list.Contains("6666")){
-        adb forward tcp:6666 tcp:6666
-    }
-    
+if (-not $adb_list.Contains("8022"))
+{
+    adb -s f8061b9  forward tcp:8022 tcp:8022
+}
+
+if (-not $adb_list.Contains("6666"))
+{
+    adb -s f8061b9  forward tcp:6666 tcp:6666
 }
