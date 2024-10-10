c909aef77b8f8753b5379c8c01ab6cfea49f93a7
chenshanlu <program_chen@foxmail.com>
2024-10-10 16:03:32 +0800

    autocommit

--- autocommit.ps1
+++ autocommit.ps1
@@ -1,11 +1,12 @@
 $port_output = Netstat -ano |findStr 127.0.0.1:6666
 
-if ($port_output -eq $null)
+if ($null -eq $port_output)
 {
+    Write-Host "no proxy config"
     $port_output = 'aaa'
 }
 
-if ( $port_output.Contains("LISTENING"))
+if ($port_output.Contains("LISTENING"))
 {
     Write-Host "Port listening... use proxy port"
     git config http.proxy http://127.0.0.1:6666
@@ -19,7 +20,7 @@ git pull
 
 git push
 
-if ( $port_output.Contains("LISTENING"))
+if ($port_output.Contains("LISTENING"))
 {
     git config --unset http.proxy
 }
