382a2c3ef73ab89d338c06b9b7033faf3033fb6b
chenshanlu <program_chen@foxmail.com>
2024-11-11 11:12:51 +0800

    autocommit

--- autocommit.ps1
+++ autocommit.ps1
@@ -10,7 +10,7 @@ if ($null -eq $port_output)
 if ($port_output.Contains("LISTENING"))
 {
     Write-Host "Port listening... use proxy port"
-    git config http.proxy http://127.0.0.1:6666
+    git config http.proxy http://procsl:asdfghjkl@www.procsl.cn:7777
 }
 
 git add  .
