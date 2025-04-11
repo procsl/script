045926d6c2cb49428989ef6650b1c1881d087981
chenshanlu <program_chen@foxmail.com>
2025-04-11 15:09:07 +0800

    autocommit

---
+++ start_cmd.cmd
@@ -0,0 +1,3 @@
+@echo off
+C:\\Users\\procsl\\AppData\\Local\\Microsoft\\WinGet\\Packages\\Google.PlatformTools_Microsoft.Winget.Source_8wekyb3d8bbwe\\platform-tools\\idea.exe -s f8061b9  forward tcp:8022 tcp:8022  
+C:\Windows\System32\OpenSSH\ssh.exe  -p 8022   u0_a330@127.0.0.1 
